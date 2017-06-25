e2f447f536a51413b804e0d3a075d73492a5c770
Lennart Hubers <97038838@st.deltion.nl>
2017-06-25 13:04:41 +0200

    foutje hihi error opgelost

--- CSharp_Project/CSharp_Project/Models/Songmodel.cs
+++ CSharp_Project/CSharp_Project/Models/Songmodel.cs
@@ -5,17 +5,20 @@ using System.Web;
 
 namespace CSharp_Project.Models
 {
+    
     public class Songmodel
     {
-        class songs
-        {
-            public string id;
-            public string artist;
-            public string song;
-            public string duration;
-            public string category;
-            public string year;
-
-        }
+       
     }
+    public class songs
+    {
+        public string id;
+        public string artist;
+        public string song;
+        public string duration;
+        public string category;
+        public string year;
+
+    }
+
 }
